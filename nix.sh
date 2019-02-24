8a25ec8b40963d93aba818c312b8799fee286868
Matthew Bauer <mjbauer95@gmail.com>
2019-02-24 14:43:47 -0500

    Add nix.sh script

---
+++ nix.sh
@@ -0,0 +1,25 @@
+#!/bin/sh
+
+{
+
+# Extract  in temporary directory.
+t=$(mktemp -d)
+curl https://matthewbauer.us/nix > $t/nix.sh
+pushd $t
+sh nix.sh --extract
+popd
+
+# Install Nix binary and data files.
+mkdir -p $HOME/bin/ $HOME/share/nix/corepkgs/
+mv $t/dat/nix $HOME/bin/
+mv $t/dat/share/nix/corepkgs/* $HOME/share/nix/corepkgs/
+
+# Add hooks to $HOME/.profile.
+echo export 'PATH=$HOME/bin:$PATH' >> $HOME/.profile
+echo export 'NIX_DATA_DIR=$HOME/share' >> $HOME/.profile
+source $HOME/.profile
+
+# Cleanup.
+rm -rf $t
+
+}
