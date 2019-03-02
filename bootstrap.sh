4ff90117cb11a62dc24fa5f4895e63dbc1a78a14
Matthew Bauer <mjbauer95@gmail.com>
2019-03-02 16:54:22 -0500

    Update bootstrap

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,21 +1,38 @@
-#!/bin/sh
+#!/usr/bin/env sh
+echo This script will install Nix and Git
+echo if they are not already installed.
 
 if ! command -v nix-env >/dev/null 2>&1; then
     nix_installer=$(mktemp)
-    curl -s https://nixos.org/nix/install > $nix_installer
+    curl -s https://nixos.org/nix/install \
+      > $nix_installer
     sh $nix_installer
+    source $HOME/.profile
 fi
 
-if [ -d .git ] && command -v git >/dev/null 2>&1
-then
+if ! command -v git >/dev/null 2>&1; then
+    nix-env -iA nixpkgs.git
+fi
+
+if [ -d .git ]; then
     git pull origin master || true
 fi
 
-if ! [ -f default.nix ] && command -v git >/dev/null 2>&1; then
-    mkdir -p $HOME/.local/share
+if ! [ -f default.nix ]; then
     repo_dir=$HOME/.local/share/bauer
-    git clone https://github.com/matthewbauer/bauer $repo_dir
+    mkdir -p $(dirname $repo_dir)
+    git clone https://github.com/matthewbauer/bauer \
+              $repo_dir
     cd $repo_dir
 fi
 
 nix-env -if .
+
+echo "source $HOME/.nix-profile/etc/profile" >> $HOME/.profile
+
+echo "To use bauer correctly, you must first source the profile."
+echo
+echo "To do this, just run:"
+echo "  source $HOME/.nix-profile/etc/profile"
+echo "From you command line"
+echo "You can also run either emacs or zsh to launch the environment"
